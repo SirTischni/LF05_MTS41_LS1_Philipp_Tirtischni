8a9708d012a541c38fa7bebe056b1716a56f5307
Philipp Tirtischni <philipp.tirtischni@viscom.de>
2025-01-23 10:52:32 +0100

    A6.3

--- Fahrkartenautomat.java
+++ Fahrkartenautomat.java
@@ -1,31 +1,75 @@
 import java.util.Scanner;
 
 class Fahrkartenautomat {
-    public static void main(String[] args) {
+    private static Scanner tastatur = new Scanner(System.in);
 
-        Scanner tastatur = new Scanner(System.in);
-        double zuZahlenderBetrag;
-        double eingezahlterGesamtbetrag;
-        double eingeworfeneMuenze;
-        double rueckgabebetrag;
-        double nochZuZahlen;
-        int anzahlTickets;
-        float ticketPreis;
+    public static void main(String[] args) {
         Enum<FahrkartentypenAB> fahrkartentypenABEnum;
 
-        //Kartentypen
-        System.out.println("Folgende Karten stehen ihnen zur Auswahl:");
-        int k = 0;
+        begruessung();
 
-        for (FahrkartentypenAB kartenTyp : FahrkartentypenAB.values())
-        {
-            System.out.println("Typ: " + kartenTyp.toString() + " Number: " + k);
-            k++;
+        fahrkartenTypen();
+
+        fahrkartentypenABEnum = auswahlFahrkartenTyp();
+
+        var zuZahlenderBetrag = farhkartenbestllungErfassung();
+
+        var eingezahlterGesamtbetrag = fahrkartenBezahlen(zuZahlenderBetrag);
+
+        fahkartenAusgabe();
+
+        rueckgeldAusgabe(eingezahlterGesamtbetrag, zuZahlenderBetrag);
+
+        System.out.println("\nVergessen Sie nicht, den Fahrschein \n" + "vor Fahrtantritt entwerten zu lassen!\n"
+                + "Wir wünschen Ihnen eine gute Fahrt.");
+
+        tastatur.close();
+    }
+
+
+
+    public static void begruessung (){
+        System.out.println("Herzlich Willkommen");
+    }
+
+    public static double farhkartenbestllungErfassung(){
+        System.out.println("\n Wie groß ist der Ticketpreis?");
+        var ticketPreis = tastatur.nextFloat();
+
+        if(ticketPreis > 10 || ticketPreis < 0) {
+            ticketPreis = 1;
+            System.out.println("Ungültige Eingabe; Ticket Preis wird auf 1 gesetzt");
         }
 
-        System.out.println("Bitte wählen Sie eine Nummer aus, die dem Ticket entspricht");
+        System.out.printf("Der Ticket Preis beträgt %f Euro", ticketPreis);
+        System.out.println("\n Wie viel Tickets wollen sie kaufen?");
+        var anzahlTickets = tastatur.nextInt();
 
+        if (anzahlTickets > 10 || anzahlTickets < 0) {
+            anzahlTickets = 1;
+            System.out.println("\n Ungültige Eingabe. Wert wird auf 1. gesetzt");
+        }
+
+        System.out.println("Ticketanzahl entspricht " + anzahlTickets);
+        return  anzahlTickets * ticketPreis;
+    }
+
+
+    public static void fahrkartenTypen(){
+    System.out.println("Folgende Karten stehen ihnen zur Auswahl:");
+
+    int k = 0;
+
+    for (FahrkartentypenAB kartenTyp : FahrkartentypenAB.values())
+    {
+        System.out.println("Typ: " + kartenTyp.toString() + " Number: " + k);
+        k++;
+    }
+    }
+
+    public static FahrkartentypenAB auswahlFahrkartenTyp(){
         while (true) {
+            System.out.println("Bitte wählen Sie eine Nummer aus, die dem Ticket entspricht");
             var kartenIntiger = tastatur.nextInt();
             if (kartenIntiger > FahrkartentypenAB.values().length || kartenIntiger < 0)
             {
@@ -36,61 +80,43 @@ class Fahrkartenautomat {
             switch(kartenIntiger)
             {
                 case 0:
-                    fahrkartentypenABEnum = FahrkartentypenAB.Kurzstrecken;
+                    return FahrkartentypenAB.Kurzstrecken;
                 case 1:
-                    fahrkartentypenABEnum = FahrkartentypenAB.Einzelfahrscheine;
+                    return FahrkartentypenAB.Einzelfahrscheine;
                 case 2:
-                    fahrkartentypenABEnum = FahrkartentypenAB.Tageskarten;
+                    return FahrkartentypenAB.Tageskarten;
                 case 3:
-                    fahrkartentypenABEnum = FahrkartentypenAB.VierFahrtenKarten;
+                    return FahrkartentypenAB.VierFahrtenKarten;
+                default:
+                    return FahrkartentypenAB.Kurzstrecken;
             }
-            break;
         }
-        //0
+    }
 
-        System.out.println("\n Wie groß ist der Ticketpreis?");
-        ticketPreis = tastatur.nextFloat();
+    public static double fahrkartenBezahlen(double zuZahlenderBetrag){
 
-        if(ticketPreis > 10 || ticketPreis < 0) {
-        ticketPreis = 1;
-        System.out.println("Ungültige Eingabe; Ticket Preis wird auf 1 gesetzt");
-        }
-
-        System.out.printf("Der Ticket Preis beträgt %f Euro", ticketPreis);
-        System.out.println("\n Wie viel Tickets wollen sie kaufen?");
-        anzahlTickets = tastatur.nextInt();
-
-        if (anzahlTickets > 10 || anzahlTickets < 0) {
-        anzahlTickets = 1;
-        System.out.println("\n Ungültige Eingabe. Wert wird auf 1. gesetzt");
-        }
-
-        System.out.println("Ticketanzahl entspricht " + anzahlTickets);
-        zuZahlenderBetrag = anzahlTickets * ticketPreis;
-
-        // 1
         System.out.printf("\n Zu zahlender Betrag : %f Euro", zuZahlenderBetrag );
 
-        // 2
-       eingezahlterGesamtbetrag = 0.0;
-       nochZuZahlen = 0.0;
+        var eingezahlterGesamtbetrag = 0.0;
+        var nochZuZahlen = 0.0;
 
         while (eingezahlterGesamtbetrag < zuZahlenderBetrag) {
             nochZuZahlen = zuZahlenderBetrag - eingezahlterGesamtbetrag;
             System.out.printf(" \n Noch zu zahlen: %d Euro", (int) nochZuZahlen);
             System.out.print("Eingabe (mind. 5 Cent, höchstens 2 Euro: ");
-            eingeworfeneMuenze = tastatur.nextDouble();
+            var eingeworfeneMuenze = tastatur.nextDouble();
 
             if (eingeworfeneMuenze > 2.00 || eingeworfeneMuenze < 0.05) {
-            System.out.print("Bitte werfen sie die Passenden Münzen ein");
+                System.out.print("Bitte werfen sie die Passenden Münzen ein");
             }
 
             eingezahlterGesamtbetrag = eingezahlterGesamtbetrag + eingeworfeneMuenze;
         }
+        return eingezahlterGesamtbetrag;
+    }
 
-        // 3
+    public static void fahkartenAusgabe(){
         System.out.println("\nFahrschein wird ausgegeben");
-
         for (int i = 0; i < 8; i++) {
             System.out.print("=");
             try {
@@ -100,10 +126,10 @@ class Fahrkartenautomat {
                 System.out.println(" Error: " + e);
             }
         }
-        System.out.println("\n\n");
+    }
 
-        // 4
-        rueckgabebetrag = eingezahlterGesamtbetrag - zuZahlenderBetrag;
+    public static void rueckgeldAusgabe(double eingezahlterGesamtbetrag, double zuZahlenderBetrag){
+        var rueckgabebetrag = eingezahlterGesamtbetrag - zuZahlenderBetrag;
         while (rueckgabebetrag > 0.0) {
             System.out.println("Der Rückgabebetrag in Höhe von " + rueckgabebetrag + " Euro");
             System.out.println(" wird in folgenden Münzen ausgezahlt:");
@@ -141,14 +167,7 @@ class Fahrkartenautomat {
 
             System.out.println("Restbetrag kleiner als 5 cent. Betrag wird auf 0 gesetzt");
             rueckgabebetrag = 0;
-
-
         }
-
-        System.out.println("\nVergessen Sie nicht, den Fahrschein \n" + "vor Fahrtantritt entwerten zu lassen!\n"
-                + "Wir wünschen Ihnen eine gute Fahrt.");
-
-        tastatur.close();
     }
 
     public enum FahrkartentypenAB {
@@ -158,4 +177,6 @@ class Fahrkartenautomat {
         VierFahrtenKarten
 
     }
+
+
 }
